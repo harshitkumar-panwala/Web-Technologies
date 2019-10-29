249439f9060814d080f6d2bb42dfa40ba08f5dee
harshitpanwala <52945164+harshitpanwala@users.noreply.github.com>
2019-10-29 23:17:56 +0530

    Add files via upload

---
+++ exam.php
@@ -0,0 +1,74 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE demo";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "demo";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+username VARCHAR(20) NOT NULL,
+password VARCHAR(20) NOT NULL,
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table Exam created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$mysqli = new mysqli("localhost", "root", "", "demo");
+ 
+// Check connection
+if($mysqli === false){
+    die("ERROR: Could not connect. " . $mysqli->connect_error);
+}
+ 
+// Escape user inputs for security
+$first_name = $mysqli->real_escape_string($_REQUEST['first_name']);
+$last_name = $mysqli->real_escape_string($_REQUEST['last_name']);
+
+// Attempt insert query execution
+$sql = "INSERT INTO exam (first_name, last_name) VALUES ('$first_name', '$last_name')";
+if($mysqli->query($sql) === true){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+}
+ 
+// Close connection
+$mysqli->close();
+?>
